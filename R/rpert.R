12f9f6b3af7e9ff103f00d804bfd565575f22c69
Jarrett Phillips <jphill01@uoguelph.ca>
2023-09-09 14:15:13 -0400

    Add files via upload

--- R/rpert.R
+++ R/rpert.R
@@ -1,14 +1,20 @@
 #' @title
-#' Generate random variates from PERT distribution. 
+#' Generate random variates from PERT distribution
 #' 
 #' @description
 #' Returns random variates sampled from a modified PERT distribution given by the following parameterization
-#'
+#' 
+#' \eqn{\mu = \frac{x_{\mathrm{min}} + x_{\mathrm{max}} + \lambda x_{\mathrm{mode}}}{\lambda + 2}}
+#' 
+#' If \eqn{\mu = x_{\mathrm{mode}}}, then \eqn{v = \frac{\lambda}{2} + 1}, else \eqn{v = \frac{(\mu - x_\mathrm{min})(2x_\mathrm{mode} - x_\mathrm{min} - x_\mathrm{max})}{(x_\mathrm{mode} - \mu)(x_\mathrm{max} - x_\mathrm{min})}}
+#' 
+#' Define \eqn{w = \frac{v(x_\mathrm{max} - \mu)}{mu - x_\mathrm{min}}}, then generate variates from a \eqn{Beta(v, w)x_\mathrm{range} + x_\mathrm{min}} distribution, where \eqn{x_\mathrm{range} = x_\mathrm{max} - x_\mathrm{min}}
+#' 
 #' @param n Number of variates to generate
 #' @param x.min Minimum x value to sample
 #' @param x.max Maximum x value to sample
 #' @param x.mode Most frequent x value to sample
-#' @param lambda Scaling factor
+#' @param lambda Scaling factor (set to 4 by default)
 #' 
 #' @returns A sample of random variates
 #' 
