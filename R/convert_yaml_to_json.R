21ad1da3c2850463bea33cccbcc69b35f91b47bf
Jarrett Phillips <jphill01@uoguelph.ca>
2023-10-11 10:32:53 -0400

    Add files via upload

--- R/convert_yaml_to_json.R
+++ R/convert_yaml_to_json.R
@@ -1,4 +1,14 @@
-convert_yaml_to_json <- function(file_path = file_path) {
+#' @title
+#' File format conversion  
+#' 
+#' @description
+#' Converts a YAML file to a JSON file
+#' 
+#' @example 
+#' # convert_yaml_to_json()
+#' 
+
+convert_yaml_to_json <- function(file_path) {
   yaml_data <- read_yaml(file.choose())
   json_data <- toJSON(yaml_data, auto_unbox = TRUE, pretty = TRUE)
   write_json(json_data, path = file_path)
