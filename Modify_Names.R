bfdf0770f9e4324066a9306bcfcb862ca4f2105a
Jarrett Phillips <jphill01@uoguelph.ca>
2024-01-11 23:13:34 -0500

    Script to modify age categories across sexes

---
+++ Modify_Names.R
@@ -0,0 +1,16 @@
+setwd("/Users/jarrettphillips/desktop")
+
+# Variables to replace
+variables_to_replace <- c("NF", "NM", "JF", "JM")
+new_variables <- c("JF", "JM", "SubAF", "SubAM")
+
+# Replace variables in the function code
+modified_code <- deparse(run_compartmental_model)
+for (i in seq_along(variables_to_replace)) {
+  modified_code <- gsub(variables_to_replace[i], new_variables[i], modified_code)
+}
+
+# Create a new function with replaced variables
+new_fun <- eval(parse(text = modified_code))
+
+dump("new_fun", file = "fun.R")
