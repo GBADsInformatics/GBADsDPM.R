3180aed30005da8c2170186c02af2fd00f5490da
Jarrett Phillips <jphill01@uoguelph.ca>
2024-01-31 10:20:58 -0500

    Add files via upload

---
+++ calculate_biomass.R
@@ -0,0 +1,23 @@
+calculate_biomass <- function() {
+
+  # Initial values
+  NF <- N_NF_t0 * lwNF
+  NM <- N_NM_t0 * lwNM
+  JF <- N_JF_t0 * lwJF
+  JM <- N_JM_t0 * lwJM
+  AF <- N_AF_t0 * lwAF
+  AM <- N_AM_t0 * lwAM
+  
+  res <- list("Neonatal Females" = NF,
+              "Neonatal Males" = NM,
+              "Juvenile Females" = JF,
+              "Juvenile Males" = JF,
+              "Adult Males" = AF,
+              "Adult Males" = AM)
+  return(res)
+
+}
+
+
+calculate_biomass()
+
