ee60a4845c335ef8d462540bc4b2065748a354e1
Jarrett Phillips <jphill01@uoguelph.ca>
2024-02-06 09:54:13 -0500

    Add files via upload

--- R/rpert.R
+++ R/rpert.R
@@ -40,7 +40,7 @@ rpert <- function(n, x_min, x_max, x_mode, lambda = 4) {
   }
   else {
     v <- ((mu - x_min) * (2 * x_mode - x_min - x_max)) /
-      ((x_mode - mu) * (x_max - x_min))
+      ((x_mode - mu) * x_range)
   }
   
   w <- (v * (x_max - mu)) / (mu - x_min)
