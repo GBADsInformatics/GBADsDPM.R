5be96607482fa8b6e8af7cbd79cc44c56284b931
Jarrett Phillips <jphill01@uoguelph.ca>
2024-02-07 19:07:13 -0500

    Add files via upload

--- load.R
+++ load.R
@@ -1,37 +1,27 @@
-# Set working directory 
-
 setwd("/Users/jarrettphillips/desktop/GBADs Postdoc/GBADs R Package/R")
-
-# Install required packages
 
 # install.packages("mc2d) # for 2D Monte Carlo simulations
 # install.packages("truncnorm") # for truncated Normal distribution
 # install.packages("yaml") # for importing YAML files
 
-# Load required packages
-
 library(mc2d)
 library(truncnorm)
 library(yaml)
 
-# Import GBADs functions
-
 source("rpert.R")
 source("read_params.R")
-source("run_compartmental_model_V1.0.R")
+source("run_compartmental_model.R")
 
-# Specify required arguments
-file_path <- "/Users/jarrettphillips/desktop/GBADs Postdoc/Ethiopia AHLE/YAML Parameter Files/params_cattle.yaml"
+file_path <- "/Users/jarrettphillips/desktop/GBADs Postdoc/Ethiopia AHLE/YAML Parameter Files/tbp_cattle/cattle_trial_CLM_all_mortality_zero.yaml"
+# file_path <- "/Users/jarrettphillips/desktop/GBADs Postdoc/Ethiopia AHLE/YAML Parameter Files/params_cattle.yaml"
 # file_path <- "/Users/jarrettphillips/desktop/GBADs Postdoc/Ethiopia AHLE/YAML Parameter Files/params_small_ruminants.yaml"
 # file_path <- "/Users/jarrettphillips/desktop/GBADs Postdoc/Ethiopia AHLE/YAML Parameter Files/params_poultry.yaml"
 
-file_type <- "yaml"
+file_type <- "yaml" # this will always be the case 
 
-seed_value <- NULL # set random seed for reproducibility
+seed_value <- NULL # set random seed for reproducibility - can be an integer of any length
 
-# Import scenario parameter file
 read_params(file_path = file_path, file_type = file_type)
 
-# Run model 
-system.time(run_compartmental_model()) # measure CPU time
+# system.time(run_compartmental_model()) # measure CPU time
 
