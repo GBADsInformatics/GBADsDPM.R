11cd1c685c9c8c136c6d3ef7016743e6194f8381
Jarrett Phillips <jphill01@uoguelph.ca>
2024-02-20 11:02:42 -0500

    Add files via upload

--- load.R
+++ load.R
@@ -12,7 +12,9 @@ source("rpert.R")
 source("read_params.R")
 source("run_compartmental_model.R")
 
-file_path <- "/Users/jarrettphillips/desktop/GBADs Postdoc/Ethiopia AHLE/YAML Parameter Files/tbp_cattle/cattle_trial_CLM_all_mortality_zero.yaml"
+file_path <- file.choose() # this will open a file explorer to select the file
+
+# file_path <- "/Users/jarrettphillips/desktop/GBADs Postdoc/Ethiopia AHLE/YAML Parameter Files/tbp_cattle/cattle_trial_CLM_all_mortality_zero.yaml"
 # file_path <- "/Users/jarrettphillips/desktop/GBADs Postdoc/Ethiopia AHLE/YAML Parameter Files/params_cattle.yaml"
 # file_path <- "/Users/jarrettphillips/desktop/GBADs Postdoc/Ethiopia AHLE/YAML Parameter Files/params_small_ruminants.yaml"
 # file_path <- "/Users/jarrettphillips/desktop/GBADs Postdoc/Ethiopia AHLE/YAML Parameter Files/params_poultry.yaml"
@@ -23,5 +25,18 @@ seed_value <- NULL # set random seed for reproducibility - can be an integer of 
 
 read_params(file_path = file_path, file_type = file_type)
 
-run_compartmental_model() # measure CPU time
+run_compartmental_model(seed_value = 0673227)
 
+
+  
+  
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
+                       
