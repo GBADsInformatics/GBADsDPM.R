c1e09fdb8e3ef41663141827d0d311fe62d7a368
Jarrett Phillips <jphill01@uoguelph.ca>
2024-03-19 14:39:22 -0400

    Add files via upload

--- R/setup.R
+++ R/setup.R
@@ -10,13 +10,20 @@
 #' @example
 #' # setup(seed_value = NULL)
 
-setup <- function(seed_value = NULL) {
-  setwd(selectDirectory(caption = "Select a directory to save results")) 
-  
-  file_path <- selectFile(caption = "Select a YAML file", filter = "*.yaml") 
+setup <- function(file_path, seed_value = NULL) {
+  file_names <- list.files(file_path, pattern = "*.yaml", full.names = TRUE)
   file_type <- "yaml"
   
-  read_params(file_path = file_path, file_type = file_type)
-  run_compartmental_model(seed_value = seed_value)
+  set.seed(seed_value)
+  
+  for (file_name in file_names) {
+    params <- read_params(file_path = file_name, file_type = file_type)
+    df <- run_compartmental_model(seed_value = seed_value)
+    base_file_name <- file_path_sans_ext(basename(file_name))
+    output_file <- file.path(file_path, paste0(base_file_name, "_AHLE.csv"))
+    write.csv(df, file = output_file, row.names = FALSE)
+  }
 }
+
+
 
