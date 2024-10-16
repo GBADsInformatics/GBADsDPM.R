b23204a5ac5b52d46ca2913ec48ce67c127e49de
Jarrett Phillips <jphill01@uoguelph.ca>
2024-10-16 14:26:52 -0400

    Add files via upload

--- load.R
+++ load.R
@@ -1,9 +1,12 @@
 setwd("/Users/jarrettphillips/Desktop/GBADs Postdoc/GBADs DPM R Package/R")
 
-# install.packages("mc2d) # for 2D Monte Carlo simulations
+# install.packages("mc2d") # for 2D Monte Carlo simulations
 # install.packages("truncnorm") # for truncated Normal distribution
 # install.packages("yaml") # for importing YAML files
 # install.packages("rstudioapi")
+# install.packages("tools")
+# install.packages("foreach")
+# install.packages("doParallel")
 
 library(mc2d)
 library(truncnorm)
@@ -13,6 +16,8 @@ library(tools)
 library(foreach)
 library(doParallel)
 
+# library(GBADsDPM)
+
 source("rpert.R")
 source("read_params.R")
 source("run_compartmental_model_V4_StochasticStartPopulation.R")
@@ -21,6 +26,8 @@ source("setup.R")
 file_path <- selectDirectory()
 seed_value <- NULL
 output <- "cumulative total"
-parallel <- FALSE
+parallel <- TRUE
 
+ptm <- proc.time()
 setup(file_path = file_path, seed_value = seed_value, parallel = parallel)
+proc.time() - ptm
