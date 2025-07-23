14cac7d58014cade5d111e3b88c0bf0d1098ec4b
Jarrett Phillips <jphill01@uoguelph.ca>
2025-07-23 16:32:27 -0400

    Add files via upload

--- load.R
+++ load.R
@@ -1,4 +1,4 @@
-setwd("./R")
+setwd("/Users/jarrettphillips/Desktop/GBADs Postdoc/GBADs DPM R Package/R")
 
 # install.packages("mc2d") # for 2D Monte Carlo simulations
 # install.packages("truncnorm") # for truncated Normal distribution
@@ -20,15 +20,12 @@ library(tools)
 
 source("rpert.R")
 source("read_params.R")
-# source("run_compartmental_model_V4_StochasticStartPopulation.R")
-# source("run_compartmental_model_V7.2_StochasticStartPop.R")
 source("run_compartmental_model_V7.2_StochasticStartPop_Girma.R")
 source("setup.R")
 
 file_path <- selectDirectory()
 # file_path <- "/Users/jarrettphillips/Desktop/Test 1"
 seed_value <- NULL
-output <- "cumulative total"
 parallel <- FALSE
 
 # ptm <- proc.time()
